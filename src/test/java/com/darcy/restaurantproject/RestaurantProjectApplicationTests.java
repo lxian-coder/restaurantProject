eb617e2251dcefbbde383595f3cc3dd665dbef1d
darcyxian <darcyxianau@gmail.com>
2021-05-09 09:32:28 +1000

    first commit

---
+++ src/test/java/com/darcy/restaurantproject/RestaurantProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darcy.restaurantproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
