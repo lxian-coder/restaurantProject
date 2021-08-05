ee49b5c387ae2881caeaf18b5a81a0e6abd0a148
darcyxian <darcyxianau@gmail.com>
2021-08-05 13:22:02 +1000

    added UserController

--- src/main/java/com/darcy/restaurantproject/controllers/UserController.java
+++ src/main/java/com/darcy/restaurantproject/controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.darcy.restaurantproject.controllers;
 
+import com.darcy.restaurantproject.dtos.UserGetDTO;
+import com.darcy.restaurantproject.dtos.UserPostDTO;
 import com.darcy.restaurantproject.entities.User;
+import com.darcy.restaurantproject.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Darcy Xian  5/8/21  10:52 am      restaurantProject
@@ -19,8 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserController {
+    private final UserService userService;
 
-//    @GetMapping
-//    public ResponseEntity<User> listAllUsers
+    @GetMapping
+    public ResponseEntity<List<UserGetDTO>> listAllUsers(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+    @PostMapping
+    public ResponseEntity<UserGetDTO> addNewUser(@RequestBody UserPostDTO newUser){
+        return ResponseEntity.ok(userService.addNewUser(newUser));
+    }
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserGetDTO> updateUser(@PathVariable Long id,@RequestBody UserPostDTO userPostDTO){
+        return ResponseEntity.ok(userService.updateUser(id,userPostDTO));
+    }
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return;
+    }
 
 }
